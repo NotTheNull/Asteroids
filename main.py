e6c74169c353b6fd37244141fd3367cc976e0d46
NotTheNull <objnotnull@live.com>
2025-02-18 12:26:05 -0700

    Rearranging code for better readability

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ from player import Player
 
 from constants import *
 
-def main():
-    pygame.init()
+ship = None
+screen = None
+clock = None
+start_x = SCREEN_WIDTH / 2
+start_y = SCREEN_HEIGHT / 2
 
+def main():
+    game_init()
+    game_loop()
+
+def game_init():
+    global screen
+    global clock
+    global ship
+
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0.0
-    start_x = SCREEN_WIDTH / 2
-    start_y = SCREEN_HEIGHT / 2
     ship = Player(start_x, start_y)
 
+
+def game_loop():
+    dt = 0.0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,7 +37,6 @@ def main():
         pygame.display.flip() #refreshes the screen
         time = clock.tick(60)
         dt = time / 1000 
-
 
 
 
