bf38cc94d2d20d6c3bb612bb279b77e90e6fccc6
NotTheNull <objnotnull@live.com>
2025-02-17 21:44:56 -0700

    Added Player class and drawing ship in middle of screen

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame
+from player import Player
 
 from constants import *
 
 def main():
     pygame.init()
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    start_x = SCREEN_WIDTH / 2
+    start_y = SCREEN_HEIGHT / 2
+    ship = Player(start_x, start_y)
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0,0,0))
+        ship.draw(screen)
         pygame.display.flip() #refreshes the screen
         time = clock.tick(60)
         dt = time / 1000 
