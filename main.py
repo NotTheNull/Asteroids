bd33451d8c02e275a9a3b7e9cd743cc804e12f8c
NotTheNull <objnotnull@live.com>
2025-02-17 20:03:52 -0700

    Setting up main with boilerplate starter code

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+def main():
+    print("Starting asteroids!")
+
+#NOTE: this check will make sure that the code will only run if the file is executed directly
+#  it will not run if this file is imported into another file
+if __name__ == "__main__":
+    main()
+
