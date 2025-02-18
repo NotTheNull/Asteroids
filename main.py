681cb65d80ef430797b02f5e2257bd9e5388456f
NotTheNull <objnotnull@live.com>
2025-02-17 20:40:46 -0700

    Setting up pygame screen

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import pygame
 from constants import *
 
 def main():
-    msg = f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}"
-    print(msg)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+        pygame.display.flip() #refreshes the screen
+
+
+
 
 #NOTE: this check will make sure that the code will only run if the file is executed directly
 #  it will not run if this file is imported into another file
