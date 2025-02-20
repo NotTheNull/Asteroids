84922c8a77456a2fd9e36a74696c9faaccea1df2
NotTheNull <objnotnull@live.com>
2025-02-19 21:02:57 -0700

    Placed limit on shooting speed to reduce number of bullets per frame

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
 
         self.rotation = 0
+        self.shoot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -18,6 +19,10 @@ class Player(CircleShape):
         return [a, b, c]
 
     def shoot(self):
+        if self.shoot_timer > 0.0:
+            return
+        
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         blast = Shot(self.position.x, self.position.y)
         blast.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
@@ -35,6 +40,8 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        if self.shoot_timer > 0.0:
+            self.shoot_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
